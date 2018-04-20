4f69c3aa37811266ace43b50157f8f4ed3fc499a
The Turkey <turkey@turkey.com>
2018-04-20 15:51:04 -0500

    ihkiiuhu

--- src/main/java/com/storagemanager/storagemanager/HelloController.java
+++ src/main/java/com/storagemanager/storagemanager/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @RequestMapping("/")
     public String hello() {
-       return "hello Nic how are you?";
+       return "hello Turkey how are you?";
    }
 }
